3c16ec7f743dfd4e5384d30d732c43b8cc4c7d1d
Philipp Holz <philippwood@gmx.net>
2022-03-04 17:26:49 +0100

    reworked

--- creates3.py
+++ creates3.py
@@ -4,9 +4,8 @@ from botocore.exceptions import ClientError
 s3_client = boto3.client('s3', 'eu-central-1')
 def create_bucket(bucket_name, region):
     try:
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,
-                                    CreateBucketConfiguration=location)
+        location = {'LocationConstraint': region}
+        s3_client.create_bucket(Bucket=bucket_name,CreateBucketConfiguration=location)
     except ClientError as e:
         logging.error(e)
         return False
--- main.py
+++ main.py
@@ -5,4 +5,3 @@ from creates3 import upload_json
 create_bucket("bucket-fmv-446", "eu-central-1")
 catch_cases("austria") 
 upload_json("bucket-fmv-446","rawdata.json")
-
